5cea0623a97fcecfefb13dfb6cf6d233101db338
Carmen <carmen1stowe@gmail.com>
2018-04-12 09:32:03 -0500

    First commit

---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/marshmellies/AubieActivity.java
@@ -0,0 +1,13 @@
+package edu.auburn.eng.csse.comp3710.spring2018.marshmellies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AubieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aubie);
+    }
+}
