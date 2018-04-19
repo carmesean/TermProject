f9220c0efac45b886cb5bf75ebfc7374b07e3c06
J Simz <JSimz@users.noreply.github.com>
2018-04-19 18:13:15 -0500

    testing

---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/marshmellies/StartGameActivity.java
@@ -0,0 +1,13 @@
+package edu.auburn.eng.csse.comp3710.spring2018.marshmellies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aubie);
+    }
+}
