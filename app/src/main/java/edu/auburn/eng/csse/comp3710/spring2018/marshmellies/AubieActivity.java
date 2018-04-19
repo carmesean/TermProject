b3ec8f1495f29e2329293d87a7f3491287995f10
Carmen <carmen1stowe@gmail.com>
2018-04-19 10:20:19 -0500

    Second commit

--- app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/marshmellies/AubieActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/marshmellies/AubieActivity.java
@@ -3,11 +3,22 @@ package edu.auburn.eng.csse.comp3710.spring2018.marshmellies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AubieActivity extends AppCompatActivity {
+    ArrayList<SimonButton> mSequenceList;   // Sequence list in the order the buttons sound
+    SimonButton mNextButton;                // Most recently sounded button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aubie);
     }
+
+    public void ammendSequence() {
+
+    }
+
 }
+
+
---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/marshmellies/SimonButton.java
@@ -0,0 +1,18 @@
+package edu.auburn.eng.csse.comp3710.spring2018.marshmellies;
+
+/**
+ * Created by kaeul on 4/19/18.
+ */
+
+public class SimonButton {
+    private String mColor;      //color
+
+    //constructor
+    public SimonButton (String color) {
+        mColor = color;
+    }
+
+    public String getColor() {
+        return mColor;
+    }
+}
