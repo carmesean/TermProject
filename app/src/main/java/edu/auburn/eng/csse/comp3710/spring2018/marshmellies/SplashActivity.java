dac6f77c864e78e5ffd9294aa193970262057f12
Carmen <carmen1stowe@gmail.com>
2018-04-24 15:16:38 -0500

    Working Load Screen

--- app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/marshmellies/SplashActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/marshmellies/SplashActivity.java
@@ -10,7 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, AubieActivity.class);
+        Intent intent = new Intent(this, StartGameActivity.class);
         startActivity(intent);
         finish();
 //        setContentView(R.layout.activity_splash);
