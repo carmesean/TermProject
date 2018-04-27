7016fedd64bf3d143e182a1d60af5d41330e8c07
Carmen <carmen1stowe@gmail.com>
2018-04-27 12:01:39 -0500

    Button Sounds Added

--- app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/marshmellies/StartGameActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/marshmellies/StartGameActivity.java
@@ -1,14 +1,17 @@
 package edu.auburn.eng.csse.comp3710.spring2018.marshmellies;
 
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import android.view.animation.Animation;
+
 
 
 public class StartGameActivity extends AppCompatActivity {
@@ -18,6 +21,10 @@ public class StartGameActivity extends AppCompatActivity {
     Button mRedButton;
     Button mYellowButton;
     Button mGreenButton;
+    MediaPlayer blueMedia;
+    MediaPlayer redMedia;
+    MediaPlayer yellowMedia;
+    MediaPlayer greenMedia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +37,62 @@ public class StartGameActivity extends AppCompatActivity {
         mYellowButton = findViewById(R.id.bottom_right_button);
         mGreenButton = findViewById(R.id.bottom_left_button);
 
-        Animation anim = new AlphaAnimation(0.0f, 1.0f);
-        anim.setDuration(50);
-        anim.setStartOffset(20);
-        anim.setRepeatMode(Animation.REVERSE);
-        anim.setRepeatCount(Animation.INFINITE);
-        mBlueButton.startAnimation(anim);
+//
+//        Animation anim = new AlphaAnimation(0.0f, 1.0f);
+//        anim.setDuration(50);
+//        anim.setStartOffset(20);
+//        anim.setRepeatMode(Animation.REVERSE);
+//        anim.setRepeatCount(Animation.INFINITE);
+//        mBlueButton.startAnimation(anim);
+
+        mBlueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (blueMedia == null) {
+                    //create a blue button media player
+                    blueMedia = MediaPlayer.create(StartGameActivity.this, R.raw.blue_sound);
+                }
+                //sound the blue button
+                blueMedia.start();
+            }
+        });
+
+        mRedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (redMedia == null) {
+                    //create a red button media player
+                    redMedia = MediaPlayer.create(StartGameActivity.this, R.raw.red_sound);
+                }
+                //sound the red button
+                redMedia.start();
+
+            }
+        });
+
+        mYellowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (yellowMedia == null) {
+                    //create a yellow button media player
+                    yellowMedia = MediaPlayer.create(StartGameActivity.this, R.raw.yellow_sound);
+                }
+                //sound the yellow button
+                yellowMedia.start();
+            }
+        });
+
+        mGreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (greenMedia == null) {
+                    //create a green button media player
+                    greenMedia = MediaPlayer.create(StartGameActivity.this, R.raw.green_sound);
+                }
+                //sound the green button
+                greenMedia.start();
+            }
+        });
 
     }
 
@@ -43,5 +100,6 @@ public class StartGameActivity extends AppCompatActivity {
         //whenever the user presses a button, add button identifier to sequence list
         mSequence.getUserSequence().add(identifier);
     }
+
 
 }
